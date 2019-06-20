d14dd88b11c377ae9b52366eded12fe3d50486a3
Preston Whitehall <pmwhitehall@gmail.com>
2019-06-20 16:04:28 -0700

    added tweet box containing unique tweet counts and handle as separate element

--- app.js
+++ app.js
@@ -4,9 +4,15 @@
 let tweetBoxMaker = () => {
   let count = 0;
   return function() {
-
+    var $tweet = $('<div class="tweets" id="tweet' + count + '"></div>');
+    $(".container").prepend($tweet);
+    let output = "#tweet" + count;
+    count++;
+    return output;
   }
 }
+
+let makeTweetBox = tweetBoxMaker();
 
 $(document).ready(function(){
   var $body = $('container');
@@ -26,17 +32,23 @@ let newTweets = () => {
     console.log("Index: " + homeLength + " StoredIndex: " + storedIndex);
     while(currentLocation <= homeLength){
       var tweet = streams.home[currentLocation];
-      var $tweet = $('<div class="tweets"></div>');
+      var tweetId = makeTweetBox();
 
       var $twitterHandle = $('<div class="twitterHandle"></div>');
+      var $message = $('<div class="message"></div>');
 
 
       // $tweet.html('@' + tweet.user + '<br>' + tweet.message + '<br>' + tweet.created_at);
-      $tweet.html(tweet.message);
+      // $tweet.html(tweet.message);
       $twitterHandle.html(tweet.user);
-      $(".container").prepend($tweet);
+      $message.html(tweet.message);
 
-      $(".tweets").prepend($twitterHandle);
+      $(tweetId).prepend($twitterHandle);
+      $(tweetId).append($message);
+      //tweetId).prepend(message);
+      //tweetId).prepend(time);
+
+      console.log(tweetId + "'@' " + tweet.user + " " + tweet.message + " " + tweet.created_at);
 
       currentLocation += 1;
     }
